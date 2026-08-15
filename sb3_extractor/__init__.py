3b792e0a82479ff2fb740b824902bf1e9fe7ff89
Lee Bush <github@waterimp.org>
2026-08-15 10:58:08 -0700

    display reason for rasterization failure. clean up code style a bit.

--- sb3_extractor/__init__.py
+++ sb3_extractor/__init__.py
@@ -48,7 +48,11 @@ def beautify_path_fragment(path):
 def rasterize_png(svg_filename):
     prefix, _ = os.path.splitext(svg_filename)
     output_filename = prefix + '-rasterized.png'
-    cairosvg.svg2png(url=svg_filename, write_to=output_filename)
+    try:
+        cairosvg.svg2png(url=svg_filename, write_to=output_filename)
+    except Exception as e:
+        print(f'  ! could not rasterize {os.path.basename(svg_filename)}: {e}')
+        return
     print(f'  * rasterized {output_filename}')
 
 
@@ -63,16 +67,15 @@ def extract_sb3(filename):
 
     for sprite in sprites:
         sprite_name = replace_delimiters(sprite.name)
-        scripts = sprite.block_info.scripts()
-        blocks = sprite.block_info.blocks()
         costumes = sprite.costumes
         sounds = sprite.sounds
         for costume_index, costume in enumerate(costumes):
             costume_name = replace_delimiters(costume.name)
             costume_center = (costume.center_x, costume.center_y)
             costume_filename = costume.filename
+            costume_extension = os.path.splitext(costume_filename)[1]
 
-            new_filename = f'{sprite_name}-{str(costume_index).zfill(3)}-{costume_name}{os.path.splitext(costume_filename)[1]}'
+            new_filename = f'{sprite_name}-{str(costume_index).zfill(3)}-{costume_name}{costume_extension}'
             new_filename = beautify_path_fragment(new_filename)
             new_filename = sanitize_path_fragment(new_filename)  # important for security
             new_filename = os.path.join(base_folder, new_filename)
@@ -84,7 +87,8 @@ def extract_sb3(filename):
             with open(new_filename, 'wb') as output_file:
                 output_file.write(contents)
 
-            rasterize_png(new_filename)
+            if costume_extension.lower() == '.svg':
+                rasterize_png(new_filename)
 
         for sound_index, sound in enumerate(sounds):
             sound_name = replace_delimiters(sound.name)
@@ -96,11 +100,12 @@ def extract_sb3(filename):
             new_filename = os.path.join(base_folder, new_filename)
 
             print(f'  * extracted {new_filename}')
-            
+
             contents = assets_map[sound_filename].read()
 
             with open(new_filename, 'wb') as output_file:
                 output_file.write(contents)
+
 
 if __name__ == '__main__':
     main()
